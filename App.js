8625f6839fc1a9787854b84772be0dc24a38abda
Guillaume Caugant <caugant@et.esiea.fr>
2021-12-11 00:37:52 +0100

    Recuperation des coordonnés de l'utilisateur

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Location from "expo-location"
+import Constants from 'expo-location'
 export default function App() {
+
+  const [location, setLocation] = useState(null)
+  const getCoordinates = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync()
+    if (status !== "granted"){
+      return
+    }
+
+    const userLocation = await Location.getCurrentPositionAsync()
+    setLocation(userLocation)
+  }
+
+  useEffect(() => {  
+    getCoordinates()
+  } , [])
+
+  if(!location){
+    return <View>
+      <Text> Aucune postion disponible</Text>
+      </View>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> latitude : {location.coords.latitude}</Text>
+      <Text> longitude : {location.coords.longitude}</Text>
     </View>
   );
 }
