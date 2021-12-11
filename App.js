601cebd8847d9b6fa0b7de1f13e0be0ae2368279
Guillaume Caugant <caugant@et.esiea.fr>
2021-12-11 01:06:21 +0100

    Appel API pour les infos météo

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import * as Location from "expo-location"
 import Constants from 'expo-location'
+import axios from "axios"
 export default function App() {
 
-  const [location, setLocation] = useState(null)
+  const URL = (lat,lon) =>  `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c7dabbcd29381e212de4ff5af5f26f7a&lan=fr&units=metric`
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
   const getCoordinates = async () => {
     const { status } = await Location.requestForegroundPermissionsAsync()
     if (status !== "granted"){
@@ -13,23 +17,36 @@ export default function App() {
     }
 
     const userLocation = await Location.getCurrentPositionAsync()
-    setLocation(userLocation)
+    getWeather(userLocation)
+  }
+
+  const getWeather = async (location) => {
+    try{
+      const reponse = await axios.get(URL(location.coords.latitude,location.coords.longitude))
+      setLoading(false)
+      setData(reponse.data)
+    }
+    catch(e){
+      console.log(e)
+    }
+    
   }
 
   useEffect(() => {  
     getCoordinates()
   } , [])
 
-  if(!location){
+
+
+  if(loading){
     return <View>
-      <Text> Aucune postion disponible</Text>
+      <ActivityIndicator/>
       </View>
   }
 
   return (
     <View style={styles.container}>
-      <Text> latitude : {location.coords.latitude}</Text>
-      <Text> longitude : {location.coords.longitude}</Text>
+      <Text> {data?.city?.name}</Text>
     </View>
   );
 }
